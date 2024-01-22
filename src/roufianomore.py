ec6635f88454739b74d8a1812b57044f008d0c63
Fotios Pechlivanis <fotispechli@gmail.com>
2024-01-22 22:50:03 +0200

    Add Check In/ Check Out requests

--- src/roufianomore.py
+++ src/roufianomore.py
@@ -8,18 +8,16 @@ load_dotenv()
 
 LOGIN_URL = 'https://ergani.softone.gr/Login/flexcar'
 LOGIN_URL_HANDLER = 'https://ergani.softone.gr/Login/flexcar?handler=Login'
-CHECK_IN_OUT_URL = 'http://127.0.0.1:5000/CheckIn/flexcar'
+CHECK_IN_OUT_URL = 'https://ergani.softone.gr/CheckIn/flexcar'
 USERNAME = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
+LANG_PAYLOAD = {'SelectedLang': 'el'}
 
 logging.basicConfig(level=logging.INFO)
 
 def login(session):
-    lang_payload = {'SelectedLang': 'el'}
-    
-    header = {
+    login_page_headers = {
         'Host': 'ergani.softone.gr',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
         'Accept-Language': 'en-US,en;q=0.5',
         'Accept-Encoding': 'gzip, deflate, br',
@@ -34,24 +32,24 @@ def login(session):
         'Sec-Fetch-Site': 'same-origin',
         'Sec-Fetch-User': '?1',
         'Sec-GPC': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0',
     }
     
     try:
-        login_page_response = session.get(LOGIN_URL, headers=header, verify=False)
+        login_page_response = session.get(LOGIN_URL, headers=login_page_headers, verify=False)
         login_page_response.raise_for_status()
 
         soup = BeautifulSoup(login_page_response.text, 'html.parser')
         anti_forgery_token = soup.find('input', {'name': '__RequestVerificationToken'}).get('value')
     except requests.RequestException as e:
         logging.error(f"Error during login page request: {e}")
-        return False
+        return login_page_response
 
-    headers = {
+    login_headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
         'Accept-Encoding': 'gzip, deflate, br',
         'Accept-Language': 'en-US,en;q=0.5',
         'Connection': 'keep-alive',
-        'Content-Length': str(287),
         'Content-Type': 'application/x-www-form-urlencoded',
         'Cookie': '; '.join([f'{name}={value}' for name, value in session.cookies.items()]),
         'DNT': '1',
@@ -77,21 +75,43 @@ def login(session):
     }
     
     try:
-        response = session.post(LOGIN_URL_HANDLER, data={**lang_payload, **credentials_payload}, headers=headers, verify=False)
+        response = session.post(LOGIN_URL_HANDLER, data={**LANG_PAYLOAD, **credentials_payload}, headers=login_headers, verify=False)
         response.raise_for_status()
-
-        # logging.info("Request Body: %s", response.request.body)
-        # logging.info("Response Text: %s", response.text)
-        return response.status_code == 200
     except requests.RequestException as e:
         logging.error(f"Error during login request: {e}")
-        return False
+    return response
 
-def check_in_out(session, check_type):
+def check_in_out(session, check_type, token):
     check_url = f"{CHECK_IN_OUT_URL}?handler={check_type}"
     
+    check_in_out_headers = {
+        'Host': 'ergani.softone.gr',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Referer': 'https://ergani.softone.gr/CheckIn/flexcar',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Origin': 'https://ergani.softone.gr',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Cookie': '; '.join([f'{name}={value}' for name, value in session.cookies.items()]),
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-User': '?1',
+        'Sec-GPC': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0',
+    }
+    
+    data_payload = {
+        'TimezoneOffset': '-120',
+        'Mode': '',
+        '__RequestVerificationToken': token,
+    }
+    
     try:
-        response = session.post(check_url)
+        response = session.post(check_url, data={**LANG_PAYLOAD, **data_payload}, headers=check_in_out_headers, verify=False)
         response.raise_for_status()
         return response.status_code
     except requests.RequestException as e:
@@ -100,15 +120,17 @@ def check_in_out(session, check_type):
 
 def main(check_type):
     session = requests.Session()
-    response = login(session)
-    print(response)
+    login_response = login(session)
     
-    # if login(session):
-    #     logging.info(f"Successfully logged in for {check_type} at {datetime.now()}")
-    #     # Check in or out
-    #     if check_in_out(session, check_type):
-    #         logging.info(f"{check_type} successful!")
-    #     else:
-    #         logging.error(f"{check_type} failed!")
-    # else:
-    #     logging.error("Login failed!")
+    print(login_response)
+    
+    if login_response.status_code == 200:
+        soup = BeautifulSoup(login_response.text, 'html.parser')
+        anti_forgery_token = soup.find('input', {'name': '__RequestVerificationToken'}).get('value')
+
+        if check_in_out(session, check_type, anti_forgery_token):
+            logging.info(f"{check_type} successful!")
+        else:
+            logging.error(f"{check_type} failed!")
+    else:
+        logging.error("Login failed!")
